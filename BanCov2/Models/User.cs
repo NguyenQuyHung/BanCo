917a64eaa76172f8424bebe22352ab6818d33849
Quý Hùng <q.hung19072003@gmail.com>
2024-11-01 12:00:33 +0700

    Add project files.

---
+++ BanCov2/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BanCov2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // Chú ý, nên mã hóa mật khẩu!
+        public string Email { get; set; }
+    }
+}
